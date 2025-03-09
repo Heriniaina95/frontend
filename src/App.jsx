414c3f6ac3cdd0dfa1c23eb8b273e4367e109d49
Heriniaina95 <andrianaryrazafi95@gmail.com>
2025-03-10 02:04:57 +0300

    rajout backgroung

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'; // Assurez-vous d'avoir un fichier CSS pour les styles
 
 const App = () => {
   const [symptoms, setSymptoms] = useState([]);
   const [countries, setCountries] = useState([]);
   const [continents, setContinents] = useState([]);
-  const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+  const [selectedSymptoms, setSelectedSymptoms] = useState({});
   const [selectedCountry, setSelectedCountry] = useState('');
   const [selectedContinent, setSelectedContinent] = useState('');
-  const [environmentSurvey, setEnvironmentSurvey] = useState({});
+  const [environmentSurvey, setEnvironmentSurvey] = useState({
+    "Type de Logement": '',
+    "Proximité de l'Eau": '',
+    "Environnement Naturel": '',
+    "Conditions Sanitaires": '',
+    "Présence d'Animaux": ''
+  });
   const [predictedDisease, setPredictedDisease] = useState('');
   const [imagePrediction, setImagePrediction] = useState(null);
+  const [showImageUpload, setShowImageUpload] = useState(false);
 
   useEffect(() => {
     // Récupérer les symptômes
@@ -47,23 +55,29 @@ const App = () => {
   };
 
   const handlePredictDisease = () => {
-    // Prédire l'environnement à partir du questionnaire
-    axios.post('http://localhost:5000/predict-environment-survey', environmentSurvey)
+    // Vérifiez que toutes les données nécessaires sont présentes
+    if (Object.values(selectedSymptoms).length === 0 || !selectedCountry ||
+        !environmentSurvey["Type de Logement"] || !environmentSurvey["Proximité de l'Eau"] ||
+        !environmentSurvey["Environnement Naturel"] || !environmentSurvey["Conditions Sanitaires"] ||
+        !environmentSurvey["Présence d'Animaux"]) {
+      console.error("Veuillez remplir tous les champs nécessaires.");
+      return;
+    }
+
+    // Prédire la maladie avec les symptômes, le pays et les réponses du questionnaire environnemental
+    const data = {
+      symptoms: Object.values(selectedSymptoms),
+      country: selectedCountry,
+      environment_survey: environmentSurvey
+    };
+
+    axios.post('http://localhost:5000/predict-disease', data)
       .then(response => {
-        const predictedEnvironment = response.data.predicted_environment;
-
-        // Prédire la maladie avec les symptômes, le pays et l'environnement prédit
-        const data = {
-          symptoms: Object.values(selectedSymptoms),
-          country: selectedCountry,
-          environment: predictedEnvironment
-        };
-
-        axios.post('http://localhost:5000/predict-disease', data)
-          .then(response => setPredictedDisease(response.data.predicted_disease))
-          .catch(error => console.error('Erreur lors de la prédiction de la maladie:', error));
+        setPredictedDisease(response.data.predicted_disease);
+        setShowImageUpload(true); // Afficher l'option de téléchargement d'image
+        console.log("Image upload should be visible:", showImageUpload); // Ajout d'un log pour vérifier l'état
       })
-      .catch(error => console.error('Erreur lors de la prédiction de l\'environnement:', error));
+      .catch(error => console.error('Erreur lors de la prédiction de la maladie:', error));
   };
 
   const handleImageUpload = (event) => {
@@ -82,16 +96,17 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="background-image"></div>
       <h1>Évaluation Intelligente des Maladies Tropicales</h1>
-      <div>
+      <div className="section">
         <h2>Sélectionnez vos symptômes</h2>
-        {Object.keys(symptoms).map(symptom => (
-          <div key={symptom}>
+        {Object.keys(symptoms).map((symptom, index) => (
+          <div key={`${symptom}-${index}`} className="form-group">
             <label>{symptom}</label>
-            <select onChange={(e) => handleSymptomChange(symptom, e.target.value)}>
+            <select onChange={(e) => handleSymptomChange(symptom, e.target.value)} className="form-control">
               <option value="">Sélectionnez un symptôme</option>
-              {symptoms[symptom].map(value => (
-                <option key={value} value={value}>
+              {symptoms[symptom].map((value, idx) => (
+                <option key={`${value}-${idx}`} value={value}>
                   {value}
                 </option>
               ))}
@@ -99,93 +114,88 @@ const App = () => {
           </div>
         ))}
       </div>
-      <div>
+      <div className="section">
         <h2>Sélectionnez le continent</h2>
-        <select onChange={handleContinentChange}>
+        <select onChange={handleContinentChange} className="form-control">
           <option value="">Sélectionnez un continent</option>
-          {Object.keys(continents).map(continent => (
-            <option key={continent} value={continent}>
+          {Object.keys(continents).map((continent, index) => (
+            <option key={`${continent}-${index}`} value={continent}>
               {continent}
             </option>
           ))}
         </select>
       </div>
-      <div>
+      <div className="section">
         <h2>Sélectionnez le pays</h2>
-        <select onChange={handleCountryChange}>
+        <select onChange={handleCountryChange} className="form-control">
           <option value="">Sélectionnez un pays</option>
-          {countries.map(country => (
-            <option key={country} value={country}>
+          {countries.map((country, index) => (
+            <option key={`${country}-${index}`} value={country}>
               {country}
             </option>
           ))}
         </select>
       </div>
-      <div>
+      <div className="section">
         <h2>Questionnaire Environnemental</h2>
-        {/* Exemple de questions, remplacez par vos questions réelles */}
-        <div>
+        <div className="form-group">
           <label>Type de Logement</label>
-          <select name="Question 1" onChange={handleEnvironmentSurveyChange}>
+          <select name="Type de Logement" onChange={handleEnvironmentSurveyChange} className="form-control">
             <option value="">Sélectionnez une réponse</option>
-            <option value="Réponse 1">Habitation précaire ou insalubre</option>
-            <option value="Réponse 2">Logement rural</option>
-            <option value="Réponse 3">Logement urbain</option>
-            {/* Ajoutez d'autres réponses possibles */}
+            <option value="Habitation précaire ou insalubre">Habitation précaire ou insalubre</option>
+            <option value="Logement rural">Logement rural</option>
+            <option value="Logement urbain">Logement urbain</option>
           </select>
         </div>
-        <div>
+        <div className="form-group">
           <label>Proximité de l'Eau</label>
-          <select name="Question 2" onChange={handleEnvironmentSurveyChange}>
+          <select name="Proximité de l'Eau" onChange={handleEnvironmentSurveyChange} className="form-control">
             <option value="">Sélectionnez une réponse</option>
-            <option value="Réponse 1">oui</option>
-            <option value="Réponse 2">non</option>
-            {/* Ajoutez d'autres réponses possibles */}
+            <option value="oui">Oui</option>
+            <option value="non">Non</option>
           </select>
         </div>
-        <div>
+        <div className="form-group">
           <label>Environnement Naturel</label>
-          <select name="Question 3" onChange={handleEnvironmentSurveyChange}>
+          <select name="Environnement Naturel" onChange={handleEnvironmentSurveyChange} className="form-control">
             <option value="">Sélectionnez une réponse</option>
-            <option value="Réponse 1">oui</option>
-            <option value="Réponse 2">non</option>
-            {/* Ajoutez d'autres réponses possibles */}
+            <option value="oui">Oui</option>
+            <option value="non">Non</option>
           </select>
         </div>
-        <div>
+        <div className="form-group">
           <label>Conditions Sanitaires</label>
-          <select name="Question 4" onChange={handleEnvironmentSurveyChange}>
+          <select name="Conditions Sanitaires" onChange={handleEnvironmentSurveyChange} className="form-control">
             <option value="">Sélectionnez une réponse</option>
-            <option value="Réponse 1">Mauvaises</option>
-            <option value="Réponse 2">Moyennes</option>
-            <option value="Réponse 3">Bonnes</option>
-            {/* Ajoutez d'autres réponses possibles */}
+            <option value="Mauvaises">Mauvaises</option>
+            <option value="Moyennes">Moyennes</option>
+            <option value="Bonnes">Bonnes</option>
           </select>
         </div>
-        <div>
+        <div className="form-group">
           <label>Présence d'Animaux</label>
-          <select name="Question 5" onChange={handleEnvironmentSurveyChange}>
+          <select name="Présence d'Animaux" onChange={handleEnvironmentSurveyChange} className="form-control">
             <option value="">Sélectionnez une réponse</option>
-            <option value="Réponse 1">oui</option>
-            <option value="Réponse 2">non</option>
-            {/* Ajoutez d'autres réponses possibles */}
+            <option value="oui">Oui</option>
+            <option value="non">Non</option>
           </select>
         </div>
-        {/* Ajoutez d'autres questions ici */}
       </div>
-      <div>
-        <h2>Téléchargez une image : Image d'inféction sur votre peau</h2>
-        <input type="file" onChange={handleImageUpload} />
-      </div>
-      <button onClick={handlePredictDisease}>Prédire la maladie</button>
+      {showImageUpload && (
+        <div className="section">
+          <h2>Téléchargez une image (optionnel)</h2>
+          <input type="file" onChange={handleImageUpload} className="form-control" />
+        </div>
+      )}
+      <button onClick={handlePredictDisease} className="btn-predict">Prédire la maladie</button>
       {predictedDisease && (
-        <div>
+        <div className="result">
           <h3>Maladie prédite :</h3>
           <p>{predictedDisease}</p>
         </div>
       )}
       {imagePrediction !== null && (
-        <div>
+        <div className="result">
           <h3>Prédiction par image :</h3>
           <p>{imagePrediction}</p>
         </div>
